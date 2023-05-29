002dd9e5d52101237bd3e3f9d42e6966a52c79be
Your Name <you@example.com>
2023-05-29 22:32:48 +0300

    slider done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,7 +1,22 @@
-$( document ).ready(function() {
-    $('.first-screen-slider').slick();
-    
+$(document).ready(function() {
+    $('.first-screen-slider').slick({
+        dots: true,
+        speed: 1000,
+        appendArrows: ".container-arrows",
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+
+                }
+            },
+
+        ]
+
+    });
+
+
     Fancybox.bind('[data-fancybox="gallery"]', {
-	
+
     });
 });
